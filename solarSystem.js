51e99871d8364de1509d2ef0a44174a0719d54dc
Joselo GR <jgarcia@nearshoremx.com>
2019-02-19 03:27:18 -0600

    group & rotation

--- solarSystem.js
+++ solarSystem.js
@@ -1,114 +1,3 @@
-var planets = [
-    sun = {
-        radius : 80.0,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 0.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "sunmap.jpg",
-        id: "sun"
-    },
-    mercury = {
-        radius : 2.0,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 140.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "mercurymap.jpg",
-        id: "mercury"
-    },
-    venus = {
-        radius : 6.0,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 180.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "venusmap.jpg",
-        id: "venus"
-    },
-    earth = {
-        radius : 6.2,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 220.0,
-        angleRotation : 0.0,
-        moon : 1,
-        rings : 0,
-        speed : 0.0,
-        material : "earthmap1k.jpg",
-        id: "earth"
-    },
-    mars = {
-        radius : 3.5,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 260.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "mars_1k_color.jpg",
-        id: "mars"
-    },
-    jupiter = {
-        radius : 34.0,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 350.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "jupitermap.jpg",
-        id: "jupiter"
-    },
-    saturn = {
-        radius : 32.0,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 450.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "saturnmap.jpg",
-        id: "saturn"
-    },
-    uranus = {
-        radius : 10.0,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 520.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "uranusmap.jpg",
-        id: "uranus"
-    },
-    neptune = {
-        radius : 9.0,
-        widthS : 20.0,
-        heightS : 20.0,
-        distanceSun : 580.0,
-        angleRotation : 0.0,
-        moon : 0,
-        rings : 0,
-        speed : 0.0,
-        material : "neptunemap.jpg",
-        id: "neptune"
-    }
-];
-
 var planetsGroup = {
     sun: null,
     mercury: null,
@@ -118,14 +7,14 @@ var planetsGroup = {
     jupiter: null,
     saturn: null,
     uranus: null,
-    neptune: null
+    neptune: null,
+    pluto: null
 };
 
 var renderer = null, 
 scene = null, 
 camera = null,
-root = null,
-group = null;
+solarGroup = null;
 
 var duration = 30000; // ms
 var currentTime = Date.now();
@@ -137,11 +26,20 @@ function animate() {
     var fract = deltat / duration;
     var angle = Math.PI * 2 * fract;
 
-    // Rotate the sphere group about its Y axis
-    group.rotation.y += angle;
+    // Rotate the solarGroup about its Y axis
+    solarGroup.rotation.y += angle;
 
-    planetsGroup['earth'].rotation.y += angle * 5;
-    planetsGroup['earth'].rotation.x -= angle * 5;
+    // Rotate the each planet and moon about its Y axis
+    for(var i=0; i<planets.length; i++) {
+        fract = deltat / planets[i]['speed'];
+        angle = Math.PI * 2 * fract;
+        for(var j=0; j<planetsGroup[planets[i]['id']]['children'].length; j++) {
+            planetsGroup[planets[i]['id']]['children'][j].rotation.y += angle;
+        }
+        planetsGroup[planets[i]['id']].rotation.y += angle;
+        //planetsGroup[planets[i]['id']].rotation.x += angle;
+    }
+    
 }
 
 function run() {
@@ -168,42 +66,38 @@ function createScene(canvas) {
 
     // Add  a camera so we can view the scene
     camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
-    camera.position.z = 900;
+    camera.position.z = 900; //900
     scene.add(camera);
     
     // Create a group to hold all the objects
-    root = new THREE.Object3D;
+    solarGroup = new THREE.Object3D;
     
     // Add a directional light to show off the object
     var light = new THREE.DirectionalLight( 0xffffff, 1);
 
     // Position the light out from the scene, pointing at the origin
     light.position.set(.5, 0, 1);
-    root.add( light );
+    solarGroup.add( light );
 
     light = new THREE.AmbientLight ( 0xaaccbb, 0.3 );
-    root.add(light);
-    
-    // Create a group to hold the spheres
-    group = new THREE.Object3D;
-    root.add(group);
+    solarGroup.add(light);
+    //scene.add(light)
 
     for(var i=0; i<planets.length; i++) {
-        console.log(planets[i]);
-       createPlanets(planets[i]);
+        createPlanets(planets[i]);
     }
 
-    // Now add the group to our scene
-    scene.add( root );
+    // Now add the solarGroup to our scene
+    scene.add( solarGroup );
 }
 
 function rotateScene(deltax) {
-    root.rotation.y += deltax / 100;
-    console.log("rotation: 0," + root.rotation.y.toFixed(2) + ",0");
+    solarGroup.rotation.y += deltax / 100;
+    console.log("rotation: 0," + solarGroup.rotation.y.toFixed(2) + ",0");
 }
 
 function scaleScene(scale) {
-    root.scale.set(scale, scale, scale);
+    solarGroup.scale.set(scale, scale, scale);
     console.log("scale: " + scale);
 }
 
@@ -211,21 +105,26 @@ function scaleScene(scale) {
 function createPlanets(planet) {
     // Create a group to hold the moons
     planetsGroup[planet['id']] = new THREE.Object3D;
-    group.add(planetsGroup[planet['id']]);
+    planetsGroup[planet['id']].name = `group_${planet['id']}`;
+    
+    solarGroup.add(planetsGroup[planet['id']]);
 
-    var spherePlanet = createAstro(`images/${planet['material']}`, planet['radius'], planet['widthS'], planet['heightS'] );
+    var spherePlanet = createAstro(`images/${planet['material']}`, planet['radius'], planet['widthSegments'], planet['heightSegments'] );
+    spherePlanet.name = planet['id'];
 
-    planetsGroup[planet['id']].add(spherePlanet)
+    planetsGroup[planet['id']].add(spherePlanet);
     planetsGroup[planet['id']].position.set(planet['distanceSun'], 0, 0);
 
     if(planet['moon'] > 0) {
-        for(var i=1; i<=planet['moon']; i++) {
-            var sphereMoon = createAstro('images/moon_1024.jpg', planet['radius']/5, planet['widthS'], planet['heightS']);
-            sphereMoon.position.set(0, 15*i, 0);
+        // for(var i=1; i<=planet['moon']; i++) {
+        for(var i=1; i<=1; i++) {
+            var sphereMoon = createAstro('images/moon_1024.jpg', planet['radiusMoon'], planet['widthSegments'], planet['heightSegments']);
+            sphereMoon.position.set(planet['distancePlanet'], 0, 0);
+            sphereMoon.name = `moon_${i}`;
             planetsGroup[planet['id']].add(sphereMoon);
         }
     }
-    group.add(planetsGroup[planet['id']]);
+    // group.add(planetsGroup[planet['id']]);
 }
 
 function createAstro(path, radius, width, height) {
